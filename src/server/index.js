1a13a78528855fa9c6efbe2a5f0830c819c1fefd
Wouter Hisschemöller <wouter.hisschemoller@digitas.com>
2019-08-22 17:14:53 +0200

    Fetch data from GitHub API and show in list.

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import { renderToString } from "react-dom/server"
-import App from '../shared/App'
-import React from 'react'
+import React from 'react';
+import { renderToString } from 'react-dom/server';
 import serialize from 'serialize-javascript';
+import App from '../shared/App'
+import { fetchPopularRepos } from '../shared/api';
 
 const app = express()
 
@@ -15,25 +16,27 @@ app.use(cors())
 app.use(express.static('public'));
 
 app.get('*', (req, res, next) => {
-  const name = 'Wouter';
-  const markup = renderToString(
-    <App data={name} />
-  );
+  fetchPopularRepos()
+    .then((data) => {
+      const markup = renderToString(
+        <App data={data} />
+      );
 
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>SSR with RR</title>
-        <script src="/bundle.js" defer></script>
-        <script>window.__INITIAL_DATA__ = ${serialize(name)};</script>
-      </head>
-      <body>
-        <div id="app">${markup}</div>
-      </body>
-    </html>
-  `
-  );
+      res.send(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>SSR with RR</title>
+            <script src="/bundle.js" defer></script>
+            <script>window.__INITIAL_DATA__ = ${serialize(data)};</script>
+          </head>
+          <body>
+            <div id="app">${markup}</div>
+          </body>
+        </html>
+      `
+      );
+  });
 })
 
 app.listen(3000, () => {
